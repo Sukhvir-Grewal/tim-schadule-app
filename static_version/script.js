ed7a5bec23593819b57e54a5f9cefb99581f430c
sukhvir <namedsukhvir@gmail.com>
2024-08-07 01:17:32 -0300

    changed scopes

--- static_version/script.js
+++ static_version/script.js
@@ -7,7 +7,7 @@ const DISCOVERY_DOCS = [
     "https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest",
 ];
 const SCOPES = [
-    "https://www.googleapis.com/auth/calendar",
+    "https://www.googleapis.com/auth/calendar.events",
     "https://www.googleapis.com/auth/gmail.readonly",
 ];
 
