d0a08745c4b91980568d3ce2751036b748dae4f9
sukhvir <namedsukhvir@gmail.com>
2024-08-08 01:00:48 -0300

    adding debugging consoles

--- node_version/server.js
+++ node_version/server.js
@@ -1,24 +1,19 @@
-const express = require('express')
-const path = require('path')
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require('express');
+const path = require('path');
 
-const app = express()
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, "./index.html"))
-})
+const app = express();
+app.use(express.static('public'));
 
 app.get('/env', (req, res) => {
 	const envData = {
 		clientId: process.env.CLIENT_ID,
 		apiKey: process.env.API_KEY
 	};
-	console.log('Environment Variables:', envData); // Log the environment variables
-	res.json(envData);
-})
+	console.log('Environment Variables:', envData); res.json(envData);
+});
 
-app.listen(3000, () => { console.log("server started") })
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-module.exports = app
+module.exports = app;
