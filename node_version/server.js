b6236bd054006e3fea4c1c4c3a197fcbcee6c384
sukhvir <namedsukhvir@gmail.com>
2024-08-08 01:39:41 -0300

    let see now

--- node_version/server.js
+++ node_version/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/env', (req, res) => {
 	const envData = {
@@ -10,15 +13,14 @@ app.get('/env', (req, res) => {
 		apiKey: process.env.API_KEY
 	};
 	console.log('Environment Variables:', envData);
-	// res.json(envData);
-	res.send("esdfsd")
+	res.json(envData);
 });
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Catch all route to serve index.html for any other routes (if single-page app)
+// Catch all route to serve index.html for any other routes
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
